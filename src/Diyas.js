dceb25bfac0c134eff920491c92741030a26f059
Vashishth Patel <76911582+vasu-1@users.noreply.github.com>
2022-10-22 11:58:22 +0000

    event commit

---
+++ src/Diyas.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './diyas.css';
+
+function Diyas() {
+  return (
+    <div class="container">
+    <div class="diya">
+    </div>
+    <div class="message">
+      <h2 style={{"font-size":"4rem", "color":"#f1c40f"}}>
+        Happy Diwali From Replit Reps
+      </h2>
+    </div>
+  </div>
+  );
+}
+
+
+function Diyas1() {
+  return (
+    <div class="container">
+    Hapy Diwali from replit reps!
+  </div>
+  );
+}
+
+
+
+
+export {Diyas,Diyas1};
